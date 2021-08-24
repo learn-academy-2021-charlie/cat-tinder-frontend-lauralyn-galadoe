0f3e7d3f0f0b86d5a0b5e48080e29a688da00a2e
lauralyngonzalez <lauralyn.gonzalez@gmail.com>
2021-08-23 17:14:28 -0700

    added container for notFound.js

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react'
+import { Container, Row, Col } from 'reactstrap'
 
 class NotFound extends Component {
   render() {
     return (
       <>
-        <div>
-          <h3 id="notfound-h3">I am the NotFound page!</h3>
-          <img src = "https://cat-world.com/wp-content/uploads/2017/06/tips-to-find-a-missing-cat.jpg" width="50%" alt ="missing-cat" id="notfound-cat-image"/>
-        </div>
+        <Container>
+          <Row>
+            <Col>
+              <h3 id="notfound-h3">404: Page not found</h3>
+              <img src = "https://cat-world.com/wp-content/uploads/2017/06/tips-to-find-a-missing-cat.jpg" width="50%" alt ="missing-cat" id="notfound-cat-image"/>
+            </Col>
+          </Row>
+        </Container>
       </>
     )
   }
