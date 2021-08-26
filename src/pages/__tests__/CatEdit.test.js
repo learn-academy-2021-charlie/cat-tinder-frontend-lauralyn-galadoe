a10761bbb45bb47d911a841b9f4a8bfbdca2c9ae
galadoe <kgaladoe@gmail.com>
2021-08-26 12:01:38 -0700

    finished update functionality

--- src/pages/__tests__/CatEdit.test.js
+++ src/pages/__tests__/CatEdit.test.js
@@ -7,15 +7,28 @@ import CatEdit from '../CatEdit'
 Enzyme.configure({ adapter: new Adapter() })
 
 describe('when cat edit renders', () => {
-  it('theres a form' , () => {
+  it("displays a form", () => {
     let cat = {
-      name: "",
-      age: "",
-      enjoys: ""
+        name: "",
+        age: "",
+        enjoys: ""
     }
-    const catEdit = shallow(<CatEdit cat={cat} />)
 
+      const catEdit = shallow(<CatEdit cat={cat}/>)
+      const form = catEdit.find("Form")
+      expect(form.length).toEqual(1)
+  })
 
+  it("displays FormGroups for each input", () => {
+    let cat = {
+        name: "",
+        age: "",
+        enjoys: ""
+    }
+
+      const catEdit = shallow(<CatEdit cat={cat}/>)
+      const formGroup = catEdit.find("FormGroup")
+      expect(formGroup.length).toEqual(3)
   })
 
 })
