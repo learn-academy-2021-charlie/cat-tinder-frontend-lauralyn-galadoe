cc9053b5ca1a1c5511c28740c2debf47bcd8f320
lauralyngonzalez <lauralyn.gonzalez@gmail.com>
2021-08-27 15:52:57 -0700

    indentation

--- src/pages/CatShow.js
+++ src/pages/CatShow.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
-import { Container, Row, Col, Card, CardTitle, CardText, Button} from 'reactstrap'
+import { Container, Row, Col, Card, CardTitle, CardBody, CardText, Button} from 'reactstrap'
 import {NavLink, Redirect} from 'react-router-dom'
-
 
 class CatShow extends Component {
   constructor(props){
@@ -23,11 +22,17 @@ class CatShow extends Component {
         <Container className="margin">
           <Row>
             <Col sm="6">
-              <Card body>
-              {cat && <>
-              <CardTitle>Meet {cat.name}</CardTitle><CardText>Age: {cat.age}</CardText><CardText>Enjoys: {cat.enjoys}</CardText>
+
+              <Card>
+              {cat && 
+              <>
+                <CardBody>
+                  <CardTitle>{cat.name}, {cat.age}</CardTitle>
+                  <CardText>Enjoys: {cat.enjoys}</CardText>
+                </CardBody>
               </>}
               </Card>
+
             </Col>
           </Row>
           <Button className="button-style">
